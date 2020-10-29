3f245af1f82579991b0bedd71735183e0d513091
Your Name <you@example.com>
2020-10-28 19:00:29 -0700

    your message

---
+++ admin/Car.php
@@ -0,0 +1,9 @@
+<?php
+ class Car {
+     public function run() {
+         
+         echo "<h1>Hey this is running.</h1>";
+     }
+ }
+
+?>
---
+++ admin/includes/database.php
@@ -0,0 +1,48 @@
+
+<?php
+require_once("new_config.php");
+class Database {
+
+    public $connection;
+    
+    //The constructor method here helps to open up the database automatically
+      function __construct() {
+          $this->open_db_connection();
+      }
+  public function  open_db_connection(){
+      
+     // $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+      $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if($this->connection->connect_errno) {
+        die("database connection failed badly" . $this->connection->connect_error);
+    }
+  }
+
+  public function query($sql) {
+      //$result = mysqli_query($this->connection, $sql); 
+     $result = $this->connection->query($sql);
+       $this->confirm_query($result);
+       return $result;
+  }
+
+  private function confirm_query($result) {
+    if(!$result) {
+        die("Query failed" . $this->connection->error);
+    }  
+  }
+
+  public function escape_string($string) {
+   // $escaped_string = mysqli_real_escape_string($this->connection, $string);
+      $escape_string = $this->connection->real_escape_string($string);
+      return $this->connection->insert_id;
+  }
+
+}
+
+$database = new Database();
+//$database->open_db_connection();
+ 
+
+
+?>
